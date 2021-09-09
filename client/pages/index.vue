8791adb9009488eaf773073713e83959deae851b
zedparman <58415963+zedparman@users.noreply.github.com>
2021-09-09 12:19:00 +0430

    Create index.vue

---
+++ client/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nuxt />
+    <appheader />
+  </div>
+</template>
+
+<script>
+import appheader from '@/components/appheader.vue';
+  export default {
+    components: {
+      appheader
+    }
+  }
+</script>
+
+<style>
+
+</style>
