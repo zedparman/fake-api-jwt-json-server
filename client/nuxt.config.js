262d6ba5a0eeb47d0a2a98b96de4b7383fa3f9c1
zedparman <58415963+zedparman@users.noreply.github.com>
2021-09-11 14:16:26 +0430

    Update nuxt.config.js

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -70,9 +70,9 @@ export default {
         //   // autoFetch: true
         // },
         endpoints: {
-          login: { url: '/api/auth/login', method: 'post', propertyName: "meta.token" },
-          logout: { url: '/api/auth/logout', method: 'post' },
-          user: { url: '/api/auth/user', method: 'get', propertyName: "data" }
+          login: { url: '/auth/login', method: 'post', propertyName: "meta.token" },
+          logout: { url: '/auth/logout', method: 'delete' },
+          user: { url: '/auth/user', method: 'get', propertyName: "user" }
         }
       }
     }
